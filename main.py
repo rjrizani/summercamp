4cc4b91f0c888a91727acfb29026d6a8b628c333
rjrizani <rjrizani@gmail.com>
2024-10-21 16:59:07 +0800

    work well

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template,redirect,url_for
+from flask import Flask, request, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,19 +6,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SECRET_KEY'] = "ABC987"
 db = SQLAlchemy(app)
 
-#Create a table
+#create a tabel
 class students(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(100))
-    Age = db.Column(db.Integer)
+    Age = db.Column(db.String(100))
     DOB = db.Column(db.String(100))
-    Gender = db.Column(db.String(10))
+    Gender = db.Column(db.String(100))
 
-    def __init__(self, name, age,dob, gender):
+    def __init__(self,name, age, dob, gender):
         self.Name = name
         self.Age = age
         self.DOB = dob
         self.Gender = gender
+
 
 @app.route('/', methods=['GET', 'POST'])
 def add_students():
@@ -30,19 +31,17 @@ def add_students():
         student = students(name, age, dob, gender)
         db.session.add(student)
         db.session.commit()
+        print("sent")
 
         return redirect(url_for('show_details'))
+
     return render_template('add_students.html')
+
 
 @app.route('/show_details')
 def show_details():
     all_students = students.query.all()
-    return render_template('show_details.html',students=all_students)
-
-
-
+    return render_template('show_details.html', students=all_students)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        app.run(debug=True,port='8081')
+    app.run(debug=True)
